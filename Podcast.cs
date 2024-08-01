c7567ecfb81966b20d92580424abbce418876012
Gabriel Freitas <freitasgabb7@gmail.com>
2024-08-01 11:47:17 -0300

    adicao de podcast

---
+++ Podcast.cs
@@ -0,0 +1,32 @@
+﻿class Podcast
+    {
+    private List<Episodio> episodios = new();
+    public Podcast(string host, string nome)
+    {
+        Host = host;
+        Nome = nome;
+    }
+    public string Host { get; }
+    public string Nome { get; }
+    //public int TotalEpisodios => episodios.Count; // Na aula pede um TotalEpisodios mas pode ser substituido por um episodios.Count?
+    public void AdicionarEpisodio(Episodio episodio)
+    {
+        episodios.Add(episodio);
+    }
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"{Nome} - {Host}\n");
+        if (episodios.Count == 0)
+        {
+            Console.WriteLine("Nenhum episódio ainda.");
+        }
+        else
+        {
+            foreach (var ep in episodios.OrderBy(e => e.Ordem))
+            {
+                Console.WriteLine($"{ep.Resumo}\n");
+            }
+        }
+        Console.WriteLine($"O podcast possui {episodios.Count} episódios");
+    }
+}
