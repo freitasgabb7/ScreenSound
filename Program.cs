56649bea0ad5f837851ffad81fd03532bb30bc94
Gabriel Freitas <freitasgabb7@gmail.com>
2024-08-01 11:48:14 -0300

    obj funcionalidade podcast

--- Program.cs
+++ Program.cs
@@ -16,10 +16,23 @@ Musica musica2 = new Musica(sleepToken, "The Summoning", alternativeMetal)
     Disponivel = false,
 };
 
-sleepToken2023.AdicionarMusica(musica1);
-sleepToken2023.AdicionarMusica(musica2);
-sleepToken.AdicionarAlbum(sleepToken2023);
-musica1.ExibirFichaTecnica();
-musica2.ExibirFichaTecnica();
-sleepToken2023.ExibirMusicasAlbum();
-sleepToken.ExibirDiscografia();
+//sleepToken2023.AdicionarMusica(musica1);
+//sleepToken2023.AdicionarMusica(musica2);
+//sleepToken.AdicionarAlbum(sleepToken2023);
+//musica1.ExibirFichaTecnica();
+//musica2.ExibirFichaTecnica();
+//sleepToken2023.ExibirMusicasAlbum();
+//sleepToken.ExibirDiscografia();
+
+Podcast placeholder = new("Zatt", "Placeholder");
+
+Episodio ep1 = new(1, "Pilot", 46);
+
+Episodio ep2 = new(2, "Where Are We?", 43);
+
+placeholder.AdicionarEpisodio(ep1);
+ep1.AdicionarConvidados("Amanda");
+ep1.AdicionarConvidados("Gabriel");
+placeholder.AdicionarEpisodio(ep2);
+ep2.AdicionarConvidados("Phaxe");
+placeholder.ExibirDetalhes();
